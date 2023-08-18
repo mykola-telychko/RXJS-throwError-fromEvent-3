79cabab1cffededef0eac0f05a59eae17df835f4
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-19 01:05:41 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-throwError-fromEvent-3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-catcherror-withmapoperators-7searg)
---
+++ index.ts
@@ -0,0 +1,36 @@
+console.clear();
+// switchMap in example below can be replaced with mergeMap/concatMap/exhaustMap, the same behaviour applies
+import { throwError, fromEvent, of } from 'rxjs';
+import {
+  catchError,
+  tap,
+  switchMap,
+  mergeMap,
+  concatMap,
+  exhaustMap,
+} from 'rxjs/operators';
+
+const fakeRequest$ = of().pipe(
+  tap((_) => console.log('fakeRequest')),
+  throwError
+);
+
+const ContinueListening$ = fromEvent(
+  document.getElementById('continued'),
+  'click'
+).pipe(
+  switchMap((_) =>
+    fakeRequest$.pipe(catchError((_) => of('continue, keep on clicking!!!')))
+  )
+);
+
+const StopListening$ = fromEvent(
+  document.getElementById('stopped'),
+  'click'
+).pipe(
+  switchMap((_) => fakeRequest$),
+  catchError((_) => of('stop, no more requests and clicks !'))
+);
+
+ContinueListening$.subscribe(console.log);
+StopListening$.subscribe(console.log);
